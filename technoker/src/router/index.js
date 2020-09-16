a6b6bd9314e1ce89fb3f0fdf6bfd1bd4646d76c4
ardhikarn <ardhikarn@gmail.com>
2020-09-16 08:30:55 +0700

    landing page

--- technoker/src/router/index.js
+++ technoker/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Landing from '../views/Landing.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/landing',
+    name: 'Landing',
+    component: Landing
   }
 ]
 
---
+++ technoker/src/views/Landing.vue
@@ -0,0 +1,191 @@
+<template>
+  <div>
+    <b-container>
+      <b-jumbotron class="mb-0 bg-white py-5">
+        <b-row class="align-items-center">
+          <b-col cols="6">
+            <h1 class="title-banner m-0">Talenta Terbaik Negeri Untuk Perubahan Revolusi 4.0</h1>
+            <p
+              class="my-4"
+            >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro delectus inventore, necessitatibus cupiditate odit doloremque.</p>
+            <b-button href="#" class="py-3">Mulai Dari Sekarang</b-button>
+          </b-col>
+          <b-col cols="6" class="text-center">
+            <img src="@/assets/img1.png" class="landing-img" />
+          </b-col>
+        </b-row>
+      </b-jumbotron>
+      <b-jumbotron class="mb-0 bg-white py-5">
+        <b-row class="align-items-center">
+          <b-col cols="6" class="text-center">
+            <img src="@/assets/img2.png" class="landing-img" />
+          </b-col>
+          <b-col cols="6">
+            <h2 class="mb-3">Kenapa Harus Mencari Tallent di Peworld ?</h2>
+            <b-row>
+              <b-col cols="1">
+                <b-icon icon="check-circle-fill" class="h4 my-3 check1"></b-icon>
+              </b-col>
+              <b-col cols="11" class="align-self-center">Lorem ipsum dolor sit amet.</b-col>
+              <b-col cols="1">
+                <b-icon icon="check-circle-fill" class="h4 my-3 check1"></b-icon>
+              </b-col>
+              <b-col cols="11" class="align-self-center">Lorem ipsum dolor sit amet.</b-col>
+              <b-col cols="1">
+                <b-icon icon="check-circle-fill" class="h4 my-3 check1"></b-icon>
+              </b-col>
+              <b-col cols="11" class="align-self-center">Lorem ipsum dolor sit amet.</b-col>
+              <b-col cols="1">
+                <b-icon icon="check-circle-fill" class="h4 my-3 check1"></b-icon>
+              </b-col>
+              <b-col cols="11" class="align-self-center">Lorem ipsum dolor sit amet.</b-col>
+            </b-row>
+          </b-col>
+        </b-row>
+      </b-jumbotron>
+      <b-jumbotron class="mb-0 bg-white py-5">
+        <b-row class="align-items-center">
+          <b-col cols="6" class="pl-5 pr-0">
+            <h2 class="mb-3">Skill Tallent</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, porro consequatur nulla unde sit quasi?</p>
+            <b-row>
+              <b-col cols="1">
+                <b-icon icon="check-circle-fill" class="h4 my-3 check2"></b-icon>
+              </b-col>
+              <b-col cols="5" class="align-self-center">Java</b-col>
+              <b-col cols="1">
+                <b-icon icon="check-circle-fill" class="h4 my-3 check2"></b-icon>
+              </b-col>
+              <b-col cols="5" class="align-self-center">Golang</b-col>
+              <b-col cols="1">
+                <b-icon icon="check-circle-fill" class="h4 my-3 check2"></b-icon>
+              </b-col>
+              <b-col cols="5" class="align-self-center">Kotlin</b-col>
+              <b-col cols="1">
+                <b-icon icon="check-circle-fill" class="h4 my-3 check2"></b-icon>
+              </b-col>
+              <b-col cols="5" class="align-self-center">C++</b-col>
+              <b-col cols="1">
+                <b-icon icon="check-circle-fill" class="h4 my-3 check2"></b-icon>
+              </b-col>
+              <b-col cols="5" class="align-self-center">PHP</b-col>
+              <b-col cols="1">
+                <b-icon icon="check-circle-fill" class="h4 my-3 check2"></b-icon>
+              </b-col>
+              <b-col cols="5" class="align-self-center">Ruby</b-col>
+              <b-col cols="1">
+                <b-icon icon="check-circle-fill" class="h4 my-3 check2"></b-icon>
+              </b-col>
+              <b-col cols="5" class="align-self-center">Javascript</b-col>
+              <b-col cols="1">
+                <b-icon icon="check-circle-fill" class="h4 my-3 check2"></b-icon>
+              </b-col>
+              <b-col cols="5" class="align-self-center">10+ Bahasa lainnya</b-col>
+            </b-row>
+          </b-col>
+          <b-col cols="6" class="text-center">
+            <img src="@/assets/img3.png" class="landing-img" />
+          </b-col>
+        </b-row>
+      </b-jumbotron>
+    </b-container>
+    <b-jumbotron bg-variant="light">
+      <b-container>
+        <b-row>
+          <b-col cols="12" class="text-center mb-4">
+            <h2>Their Opinion About Peworld</h2>
+          </b-col>
+          <b-col cols="4" class="text-center">
+            <b-card class="landing-card border-light ml-5" style="max-width: 19rem;">
+              <b-avatar size="6rem" class="mb-1 opinion-people">
+                <img src="@/assets/opinion1.jpg" style="width: 5.5rem" class="mt-4" />
+              </b-avatar>
+              <b-card-title class="mb-0">Harry Styles</b-card-title>
+              <small class="text-muted">Web Developer</small>
+              <b-card-text
+                class="mt-2 px-5"
+              >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque ea nisi magni accusamus. Saepe sequi, culpa suscipit aut nam doloremque doloribus? Soluta laboriosam vitae odit!</b-card-text>
+            </b-card>
+          </b-col>
+          <b-col cols="4" class="text-center">
+            <b-card class="landing-card border-light mx-auto" style="max-width: 19rem;">
+              <b-avatar size="6rem" class="mb-1 opinion-people">
+                <img src="@/assets/opinion2.jpg" style="width: 5.5rem" class="mt-4" />
+              </b-avatar>
+              <b-card-title class="mb-0">Harry Styles</b-card-title>
+              <small class="text-muted">Web Developer</small>
+              <b-card-text
+                class="mt-2 px-5"
+              >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque ea nisi magni accusamus. Saepe sequi, culpa suscipit aut nam doloremque doloribus? Soluta laboriosam vitae odit!</b-card-text>
+            </b-card>
+          </b-col>
+          <b-col cols="4" class="text-center">
+            <b-card class="landing-card border-light mr-5" style="max-width: 19rem;">
+              <b-avatar size="6rem" class="mb-1 opinion-people">
+                <img src="@/assets/opinion3.jpg" style="width: 5.5rem" class="mt-4" />
+              </b-avatar>
+              <b-card-title class="mb-0">Harry Styles</b-card-title>
+              <small class="text-muted">Web Developer</small>
+              <b-card-text
+                class="mt-2 px-5"
+              >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque ea nisi magni accusamus. Saepe sequi, culpa suscipit aut nam doloremque doloribus? Soluta laboriosam vitae odit!</b-card-text>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-jumbotron>
+    <b-container>
+      <b-jumbotron class="card-bottom">
+        <b-row class="pt-2 align-items-center">
+          <b-col cols="1"></b-col>
+          <b-col cols="3" class="pl-0">
+            <h2 class="text-white">Lorem ipsum dolor sit amet.</h2>
+          </b-col>
+          <b-col cols="5"></b-col>
+          <b-col cols="3">
+            <b-button href="#" class="bg-white py-3 px-4" style="color: #5E50A1">Mulai Dari Sekarang</b-button>
+          </b-col>
+        </b-row>
+      </b-jumbotron>
+    </b-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Landing',
+  components: {}
+}
+</script>
+
+<style scoped>
+.landing-img {
+  width: 100%;
+  height: 100%;
+}
+.title-banner {
+  line-height: 55px;
+  letter-spacing: 3px;
+}
+.btn-secondary {
+  background-color: #5e50a1;
+}
+.check1 {
+  color: #5e50a1;
+}
+.check2 {
+  color: #fbb017;
+}
+.opinion-people {
+  border: 6px solid #f7d28a;
+}
+.landing-card {
+  box-shadow: 0px 8px 16px rgba(197, 197, 197, 0.25);
+}
+.card-bottom {
+  background-color: #5e50a1;
+  border-radius: 40px 8px;
+}
+</style>
