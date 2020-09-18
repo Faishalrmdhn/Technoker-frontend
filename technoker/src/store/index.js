626b2edc412392216f3cf7d7a4c52fea99b2601c
Faishalrmdhn <faishalramadhan0@gmail.com>
2020-09-18 16:48:42 +0700

    okokok

--- technoker/src/store/index.js
+++ technoker/src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import Auth from './modules/auth'
 import createPersistedState from 'vuex-persistedstate'
+import worker from './Naldi/worker'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    Auth
+    Auth,
+    worker
   },
   state: {},
   mutations: {},
